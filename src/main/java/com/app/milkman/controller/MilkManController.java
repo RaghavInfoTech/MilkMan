9e55a7ff08c831400a57745bcd5ec9ef33a71e76
Raghava <raghavareddy.dcn@gmail.com>
2023-12-30 01:44:46 +0530

    healthcheck

--- src/main/java/com/app/milkman/controller/MilkManController.java
+++ src/main/java/com/app/milkman/controller/MilkManController.java
@@ -5,6 +5,7 @@ import com.app.milkman.model.EmailRequest;
 import com.app.milkman.model.SMSRequest;
 import com.app.milkman.component.EncryptDecrypt;
 import com.app.milkman.component.SMSComponent;
+import com.app.milkman.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +22,40 @@ public class MilkManController {
     @Autowired
     private EmailComponent emailComponent;
 
-    @GetMapping("/healthCheck")
+    @GetMapping("")
     public String health() {
+
+
+        return "SUCCESS";
+    }
+
+    @GetMapping("/sms")
+    public String sms() {
+        SMSRequest sms = new SMSRequest();
+        sms.setToNumber("+919566085621");
+        sms.setTextMessage("test");
+        smsService.sendSMS(sms);
+        return "SUCCESS";
+    }
+
+    @GetMapping("/mail")
+    public String mail() {
         EmailRequest email = new EmailRequest();
         email.setRecipient("raghavareddy.dcn@gmail.com");
         email.setSubject("MILK MAN");
         email.setMsgBody("Sample mail ...");
         emailComponent.sendEmail(email);
         return "SUCCESS";
+    }
+
+
+    @GetMapping("/encrypt")
+    public String encrypt(String encrypt) {
+        return encryptDecrypt.encrypt(encrypt, Constants.KEY);
+    }
+
+    @GetMapping("/decrypt")
+    public String decrypt(String decrypt) {
+        return encryptDecrypt.decrypt(decrypt, Constants.KEY);
     }
 }
