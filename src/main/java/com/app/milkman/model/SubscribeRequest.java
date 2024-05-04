3157b9114c295429c08de7fd89aa914512665c10
Raghava <raghavareddy.dcn@gmail.com>
2024-05-05 02:04:07 +0530

    schema changes delivery date

--- src/main/java/com/app/milkman/model/SubscribeRequest.java
+++ src/main/java/com/app/milkman/model/SubscribeRequest.java
@@ -1,5 +1,6 @@
 package com.app.milkman.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,7 +11,9 @@ public class SubscribeRequest {
     private String subscriptionId;
     private String customerId;
     private List<ProductOrdersReq> productOrderReqs;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate deliveryStartDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate deliveryEndDate;
     private String deliveryTimeSlot;
     private List<String> deliveryDays;
